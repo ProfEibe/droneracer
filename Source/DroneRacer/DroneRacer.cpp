7de00d251bcc7bfe75bd5d36b6d02929a1c8db26
Jakob Schade <jakob.schade@gmail.com>
2016-08-16 16:36:22 +0200

    Clean Flying Example

---
+++ Source/DroneRacer/DroneRacer.cpp
@@ -0,0 +1,10 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "DroneRacer.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, DroneRacer, "DroneRacer");
+
+DEFINE_LOG_CATEGORY(LogFlying)
+
+ 
---
+++ Source/DroneRacer/DroneRacerGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "DroneRacerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADroneRacerGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ADroneRacerGameMode();
+};
+
+
+
