5767e1579eebc59d75346a0e0a6bac4cb5c61646
Jakob Schade <jakob.schade@gmail.com>
2016-09-29 17:39:52 +0200

    First try of new Game Mode
    Former-commit-id: 292297854555a627c6357e303cac975ec8936613

---
+++ Source/DroneRacer/DroneRacerGM.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "DroneRacerGM.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRONERACER_API ADroneRacerGM : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
