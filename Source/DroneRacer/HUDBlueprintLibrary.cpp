1dfc32cb0809d3d346c2d0e3e7c2f6424263004a
Jakob Schade <jakob.schade@gmail.com>
2017-01-12 02:19:34 +0100

    Working C++ Code (For Player 1)

--- Source/DroneRacer/HUDBlueprintLibrary.cpp
+++ Source/DroneRacer/HUDBlueprintLibrary.cpp
@@ -11,92 +11,86 @@ void UHUDBlueprintLibrary::FindScreenEdgeLocationForWorldLocation(UObject* World
 {
 	bIsOnScreen = false;
 	OutRotationAngleDegrees = 0.f;
-	FVector2D *ScreenPosition = new FVector2D();
+	FVector2D m_ScreenPosition = FVector2D(); //Todo: Check if the new is necessary
 
-	const FVector2D ViewportSize = InViewportSize;// FVector2D(GEngine->GameViewport->Viewport->GetSizeXY());
-	const FVector2D  ViewportCenter = FVector2D(ViewportSize.X / 2, ViewportSize.Y / 2);
+	const FVector2D m_ViewportSize = InViewportSize;// FVector2D(GEngine->GameViewport->Viewport->GetSizeXY());
+	const FVector2D  m_ViewportCenter = FVector2D(m_ViewportSize.X / 2, m_ViewportSize.Y / 2);
+
+	float m_invert = 1.0f;
+	float m_rotateInvert = 0.0f;
 
 	UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject);
 
 	if (!World) return;
 
-	APlayerController* PlayerController = (WorldContextObject ? UGameplayStatics::GetPlayerController(WorldContextObject, PlayerID) : NULL);
-	ACharacter *PlayerCharacter = static_cast<ACharacter *> (PlayerController->GetPawn());
+	APlayerController* m_PlayerController = (WorldContextObject ? UGameplayStatics::GetPlayerController(WorldContextObject, PlayerID) : NULL);
+	ACharacter *m_PlayerCharacter = static_cast<ACharacter *> (m_PlayerController->GetPawn());
 
-	if (!PlayerCharacter) return;
+	if (!m_PlayerCharacter) return;
 
+	FVector m_PlayerLocation = m_PlayerCharacter->GetActorLocation();
 
-	FVector Forward = PlayerCharacter->GetActorForwardVector();
-	FVector Offset = (InLocation - PlayerCharacter->GetActorLocation()).GetSafeNormal();
+	UWidgetLayoutLibrary::ProjectWorldLocationToWidgetPosition(m_PlayerController, InLocation, m_ScreenPosition);
 
-	float DotProduct = FVector::DotProduct(Forward, Offset);
-	bool bLocationIsBehindCamera = (DotProduct < 0);
+	//PlayerController->ProjectWorldLocationToScreen(InLocation, *m_ScreenPosition);
 
-	if (bLocationIsBehindCamera)
+	if (m_ScreenPosition.X == 0.0f)
 	{
-		// For behind the camera situation, we cheat a little to put the
-		// marker at the bottom of the screen so that it moves smoothly
-		// as you turn around. Could stand some refinement, but results
-		// are decent enough for most purposes.
-
-		FVector DiffVector = InLocation - PlayerCharacter->GetActorLocation();
-		FVector Inverted = DiffVector * -1.f;
-		FVector NewInLocation = PlayerCharacter->GetActorLocation() * Inverted;
-
-		NewInLocation.Z -= 5000;
-
-		PlayerController->ProjectWorldLocationToScreen(NewInLocation, *ScreenPosition);
-		ScreenPosition->Y = (EdgePercent * ViewportCenter.X) * 2.f;
-		ScreenPosition->X = -ViewportCenter.X - ScreenPosition->X;
-	}
-
-	UWidgetLayoutLibrary::ProjectWorldLocationToWidgetPosition(PlayerController, InLocation, *ScreenPosition);
-
-	// Check to see if it's on screen. If it is, ProjectWorldLocationToScreen is all we need, return it.
-	if (ScreenPosition->X >= 0.f && ScreenPosition->X <= ViewportSize.X
-		&& ScreenPosition->Y >= 0.f && ScreenPosition->Y <= ViewportSize.Y)
-	{
-		OutScreenPosition = *ScreenPosition;
-		bIsOnScreen = true;
-		return;
-	}
-
-	*ScreenPosition -= ViewportCenter;
-
-	float AngleRadians = FMath::Atan2(ScreenPosition->Y, ScreenPosition->X);
-	AngleRadians -= FMath::DegreesToRadians(90.f);
-
-	OutRotationAngleDegrees = FMath::RadiansToDegrees(AngleRadians) + 180.f;
-
-	float Cos = cosf(AngleRadians);
-	float Sin = -sinf(AngleRadians);
-
-	ScreenPosition = new FVector2D(ViewportCenter.X + (Sin * 150.f), ViewportCenter.Y + Cos * 150.f);
-
-	float m = Cos / Sin;
-
-	FVector2D ScreenBounds = ViewportCenter * EdgePercent;
-
-	if (Cos > 0)
-	{
-		ScreenPosition = new FVector2D(ScreenBounds.Y / m, ScreenBounds.Y);
+		FVector location = FMath::Lerp(InLocation, m_PlayerLocation, 10.0f);
+		m_PlayerController->ProjectWorldLocationToScreen(location, m_ScreenPosition);
+		m_invert = -1.0f;
+		m_rotateInvert = 180.0f;
 	}
 	else
 	{
-		ScreenPosition = new FVector2D(-ScreenBounds.Y / m, -ScreenBounds.Y);
+		// Check to see if it's on screen. If it is, ProjectWorldLocationToScreen is all we need, return it.
+		if (m_ScreenPosition.X >= 0 && m_ScreenPosition.X <= m_ViewportSize.X 
+			&& m_ScreenPosition.Y >= 0 && m_ScreenPosition.Y <= m_ViewportSize.Y)
+		{
+			OutScreenPosition = m_ScreenPosition;
+			OutRotationAngleDegrees = 0.0f;
+			bIsOnScreen = true;
+			return;
+		}
 	}
 
-	if (ScreenPosition->X > ScreenBounds.X)
+	m_ScreenPosition -= m_ViewportCenter;
+
+	float m_AngleRadians = FMath::Atan2(m_ScreenPosition.Y, m_ScreenPosition.X);
+	m_AngleRadians -= FMath::DegreesToRadians(90.f);
+
+	OutRotationAngleDegrees = FMath::RadiansToDegrees(m_AngleRadians) + 180.f;
+	OutRotationAngleDegrees += m_rotateInvert;
+
+	float m_cos = cosf(m_AngleRadians);
+	float m_sin = -sinf(m_AngleRadians);
+
+	//*m_ScreenPosition = FVector2D(m_ViewportCenter.X + (m_sin * 150.f), m_ViewportCenter.Y + m_cos * 150.f);
+
+	float m = m_cos / m_sin;
+
+	FVector2D ScreenBounds = m_ViewportCenter * EdgePercent;
+
+	if (m_cos > 0)
 	{
-		ScreenPosition = new FVector2D(ScreenBounds.X, ScreenBounds.X*m);
+		m_ScreenPosition = FVector2D(ScreenBounds.Y / m, ScreenBounds.Y);
 	}
-	else if (ScreenPosition->X < -ScreenBounds.X)
+	else
 	{
-		ScreenPosition = new FVector2D(-ScreenBounds.X, -ScreenBounds.X*m);
+		m_ScreenPosition = FVector2D(-ScreenBounds.Y / m, -ScreenBounds.Y);
 	}
 
-	*ScreenPosition += ViewportCenter;
+	if (m_ScreenPosition.X > ScreenBounds.X)
+	{
+		m_ScreenPosition = FVector2D(ScreenBounds.X, ScreenBounds.X*m);
+	}
+	else if (m_ScreenPosition.X < -ScreenBounds.X)
+	{
+		m_ScreenPosition = FVector2D(-ScreenBounds.X, -ScreenBounds.X*m);
+	}
 
-	OutScreenPosition = *ScreenPosition;
+	m_ScreenPosition *= m_invert;
+	m_ScreenPosition += m_ViewportCenter;
 
+	OutScreenPosition = m_ScreenPosition;
 }
